9c75aad037c79b9aeb7b62e83174bb18bbbdce4a
Firkhraag <firkhraag@gmail.com>
2019-03-25 18:08:34 +0300

    Add target folder

---
+++ Histology/cross-section-viewer/target/generated-sources/annotations/org/spbu/histology/cross/section/viewer/Bundle.java
@@ -0,0 +1,27 @@
+package org.spbu.histology.cross.section.viewer;
+/** Localizable strings for {@link org.spbu.histology.cross.section.viewer}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>CrossSectionViewer</i>
+     * @see CrossSectionViewerTopComponent
+     */
+    static String CTL_CrossSectionViewerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CrossSectionViewerAction");
+    }
+    /**
+     * @return <i>CrossSectionViewer Window</i>
+     * @see CrossSectionViewerTopComponent
+     */
+    static String CTL_CrossSectionViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CrossSectionViewerTopComponent");
+    }
+    /**
+     * @return <i>This is a CrossSectionViewer window</i>
+     * @see CrossSectionViewerTopComponent
+     */
+    static String HINT_CrossSectionViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CrossSectionViewerTopComponent");
+    }
+    private void Bundle() {}
+}
